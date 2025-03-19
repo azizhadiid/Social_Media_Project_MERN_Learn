71c0465b900375bf5c4db9fa45383fd25bd2f313
azizhadiid <azizalhadiid88@gmail.com>
2025-03-19 21:15:23 +0700

    Database Has Been Complate

--- script_SQL.sql
+++ script_SQL.sql
@@ -43,4 +43,84 @@ ADD CONSTRAINT `userId`
   REFERENCES `social`.`users` (`id`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
+  
+CREATE TABLE `social`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(200) NOT NULL,
+  `createdAt` DATETIME NULL,
+  `userId` INT NOT NULL,
+  `postId` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `postId_idx` (`postId` ASC) VISIBLE,
+  INDEX `commentUserId_idx` (`userId` ASC) VISIBLE,
+  CONSTRAINT `commentUserId`
+    FOREIGN KEY (`userId`)
+    REFERENCES `social`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `postId`
+    FOREIGN KEY (`postId`)
+    REFERENCES `social`.`posts` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE `social`.`stories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `img` VARCHAR(200) NOT NULL,
+  `userId` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `storyUserId_idx` (`userId` ASC) VISIBLE,
+  CONSTRAINT `storyUserId`
+    FOREIGN KEY (`userId`)
+    REFERENCES `social`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE `social`.`relationships` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `followerUserId` INT NOT NULL,
+  `followedUserId` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `followerUser_idx` (`followerUserId` ASC) VISIBLE,
+  INDEX `followedUser_idx` (`followedUserId` ASC) VISIBLE,
+  CONSTRAINT `followerUser`
+    FOREIGN KEY (`followerUserId`)
+    REFERENCES `social`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `followedUser`
+    FOREIGN KEY (`followedUserId`)
+    REFERENCES `social`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE `social`.`likes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `userId` INT NOT NULL,
+  `postId` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `likeUserId_idx` (`userId` ASC) VISIBLE,
+  INDEX `likePostId_idx` (`postId` ASC) VISIBLE,
+  CONSTRAINT `likeUserId`
+    FOREIGN KEY (`userId`)
+    REFERENCES `social`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `likePostId`
+    FOREIGN KEY (`postId`)
+    REFERENCES `social`.`posts` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+
+
 
