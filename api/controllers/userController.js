fd6097b246b6887e4777e981d02ceeaa3d45e99c
azizhadiid <azizalhadiid88@gmail.com>
2025-03-21 20:33:49 +0700

    Connection has been success and Register too

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,1 +1,4 @@
-// const getUser
+export const getUser = (req,res)=> {
+    // TODO
+    
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { getUser } from "../controllers/userController.js";
 
 const router = express.Router();
-router.get("/test", );
+router.get("/find/userId", getUser);
 
 export default router;
